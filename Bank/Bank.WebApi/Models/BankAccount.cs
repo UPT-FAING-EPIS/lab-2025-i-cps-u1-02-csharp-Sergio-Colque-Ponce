c9935aaed99ff2311e97d57ef340530fe19963ad
Sergio-Colque-Ponce <younever.tlv@gmail.com>
2025-04-05 09:51:18 -0500

    punto 14 docker faltante

---
+++ Bank/Bank.WebApi/Models/BankAccount.cs
@@ -0,0 +1,28 @@
+namespace Bank.WebApi.Models
+{
+    public class BankAccount
+    {
+        private readonly string m_customerName = string.Empty;
+        private double m_balance;
+        private BankAccount() { }
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+        public string CustomerName { get { return m_customerName; } }
+        public double Balance { get { return m_balance; }  }
+        public void Debit(double amount)
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(amount, m_balance);
+            ArgumentOutOfRangeException.ThrowIfNegative(amount);
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(amount);
+            m_balance += amount;
+        }
+    }
+}
